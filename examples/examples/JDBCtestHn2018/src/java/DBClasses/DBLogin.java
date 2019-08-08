c6ffe771f07c2149f8fb7691e902761405b4bfd8
Hallgeir Nilsen <hallgeir@uia.no>
2019-08-08 14:22:07 +0200

    oppdat 190808_2

---
+++ examples/examples/JDBCtestHn2018/src/java/DBClasses/DBLogin.java
@@ -0,0 +1,51 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+
+JDBC JNDI DataSource Servlet
+ */
+package DBClasses;
+
+import java.io.PrintWriter;
+import java.sql.*; 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+/**
+ *
+ * @author hallgeir
+ */
+public class DBLogin {
+    Connection conn; 
+    Statement stmt;
+    
+    
+   // @Resource DataSource LocalhostDS;
+    public Connection getConnection(PrintWriter out) {
+        try {
+         // Step 1: Allocate a database 'Connection' object
+         Context cont = new InitialContext();
+         DataSource ds = (DataSource)cont.lookup("java:comp/env/jdbc/localhostDS");  
+         //DataSource ds = (DataSource)cont.lookup("jdbc/LocalhostDS");
+         conn =  ds.getConnection();
+ 
+         // Step 2: Allocate a 'Statement' object in the Connection
+         stmt = conn.createStatement();
+        }
+        catch (SQLException ex ) {
+            out.println("Not connected to database " +ex);
+        }
+        catch (NamingException nex) {
+            out.println("Not correct naming" + nex);
+        }
+            
+        return conn;
+    }
+    
+             
+   
+}// slutt 
+    
